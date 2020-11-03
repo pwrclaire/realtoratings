603c80327f9508db63d1ec51e70d8954b903f091
Claire Peng <pwrclaire@gmail.com>
2020-11-02 21:12:06 -0800

    added auth

--- client/src/components/Homepage/MarketNews.js
+++ client/src/components/Homepage/MarketNews.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Profile } from "../Profile";
 
 class MarketNews extends Component {
   componentDidMount() {
@@ -6,54 +7,49 @@ class MarketNews extends Component {
   }
 
   state = {
-    news: ''
-  }
+    news: "",
+  };
 
-  fetchNews = () => {
-    const today = new Date().toJSON().slice(0,10).replace(/-/g,'-');
+  fetchNews = async () => {
+    const today = new Date().toJSON().slice(0, 10).replace(/-/g, "-");
     console.log(today);
-    const url = 'https://newsapi.org/v2/everything?' +
-    'q=Real Estate Market&' +
-    'from='+ today +
-    'sortBy=popularity&' +
-    'apiKey=b07149fc1f174e04b0bcfaddf3327b33';
-
-    fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(news => {
-      if(news.status === 'ok') {
-        this.setState({
-          news
-        })
-      }
-    })
-  }
+    const url = `https://newsapi.org/v2/everything?q=Real Estate Market&from=${today}sortBy=popularity&apiKey=b07149fc1f174e04b0bcfaddf3327b33`;
+    const resp = await fetch(url);
+    const news = await resp.json();
+    if (news.status === "ok") {
+      this.setState({
+        news,
+      });
+    }
+  };
 
   displayArticles = () => {
     const { articles } = this.state.news;
-    if(articles) {
+    if (articles) {
       return (
         <div>
-          <div>{articles.map(x => 
-            <p key={x.url}>
-              {x.source.name} <br/>
-              <a href={x.url}>{x.title}</a>
-            </p>
-            )}
+          <div>
+            {articles.map((x) => (
+              <p key={x.url}>
+                {x.source.name} <br />
+                <a href={x.url}>{x.title}</a>
+              </p>
+            ))}
           </div>
-          <p>-Powered by <a href="https://newsapi.org/">newsapi.org</a></p>
+          <p>
+            -Powered by <a href="https://newsapi.org/">newsapi.org</a>
+          </p>
         </div>
-      )
+      );
     }
-  }
+  };
 
   render() {
     return (
       <div>
+        <Profile />
         <h4 className="title">World Market</h4>
-          {this.displayArticles()}
+        {this.displayArticles()}
       </div>
     );
   }
---
+++ client/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-class-properties",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
+    ],
+  },
+};
